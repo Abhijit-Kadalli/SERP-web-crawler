3839326af20a57109aa30ef23762c0a34be34a42
Abhijit Kadalli <abhijitkadalli@nitgoa.ac.in>
2023-05-25 20:57:48 +0530

    script to obtain the channel link from the yt vid

---
+++ channel_extracter.py
@@ -0,0 +1,36 @@
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# Set up your API key
+api_key = 'YOUR_API_KEY'  # Replace with your actual API key
+
+# Create a YouTube service object
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+# Function to retrieve the YouTube account link from a video link
+def get_account_link(video_url):
+    try:
+        # Extract the video ID from the URL
+        video_id = video_url.split('?v=')[1]
+
+        # Call the API to get video details
+        video_response = youtube.videos().list(
+            part='snippet',
+            id=video_id
+        ).execute()
+
+        # Extract the channel ID from the video details
+        channel_id = video_response['items'][0]['snippet']['channelId']
+
+        # Construct the YouTube account link
+        account_link = f'https://www.youtube.com/channel/{channel_id}'
+
+        return account_link
+
+    except HttpError as e:
+        print('An HTTP error occurred:')
+        print(e)
+        return None
+    except KeyError:
+        print('Invalid video URL. Please provide a valid YouTube video link.')
+        return None
